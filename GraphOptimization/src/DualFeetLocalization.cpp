d08419cfbc14da7ef09808e8f78a61016e15d247
steve <551619855@qq.com>
2018-01-21 17:52:12 +0800

    dual feet localization.

--- GraphOptimization/src/DualFeetLocalization.cpp
+++ GraphOptimization/src/DualFeetLocalization.cpp
@@ -12,8 +12,27 @@
 #include "AWF.h"
 
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
+    // parameters
+    std::string dir_name = "/home/steve/Data/FusingLocationData/0017/";
 
-    std::cout << "hellow World" << std::endl;
+
+
+    // load data
+    AWF::FileReader left_foot_file(dir_name + "LEFT_FOOT.data"),
+            right_foot_file(dir_name + "RIGHT_FOOT.data"),
+            head_imu_file(dir_name + "HEAD.data"),
+            uwb_file(dir_name + "HEAD_UWB.data"),
+            beacon_set_file(dir_name + "beaconSet.csv");
+
+    auto left_imu_data = left_foot_file.extractDoulbeMatrix(",");
+    auto right_imu_data = right_foot_file.extractDoulbeMatrix(",");
+    auto head_imu_data = head_imu_file.extractDoulbeMatrix(",");
+    auto uwb_data = uwb_file.extractDoulbeMatrix(",");
+    auto beacon_set_data = uwb_file.extractDoulbeMatrix(",");
+
+
+    //process
+
 
 }
