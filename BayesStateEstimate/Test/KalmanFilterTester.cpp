0c2f26ae2b55a2a5f76e57cd7c3018c6a218c95f
steve <551619855@qq.com>
2018-01-24 23:18:36 +0800

    dual feet localization.

---
+++ BayesStateEstimate/Test/KalmanFilterTester.cpp
@@ -0,0 +1,38 @@
+//
+// Created by steve on 18-1-24.
+//
+
+
+#include <iostream>
+#include <fstream>
+
+
+#include <thread>
+
+#include "AWF.h"
+
+
+int main(int argc, char *argv[]) {
+    // parameters
+    std::string dir_name = "/home/steve/Data/FusingLocationData/0017/";
+
+
+
+    // load data
+    AWF::FileReader left_foot_file(dir_name + "LEFT_FOOT.data"),
+            right_foot_file(dir_name + "RIGHT_FOOT.data"),
+            head_imu_file(dir_name + "HEAD.data"),
+            uwb_file(dir_name + "HEAD_UWB.data"),
+            beacon_set_file(dir_name + "beaconSet.csv");
+
+    auto left_imu_data = left_foot_file.extractDoulbeMatrix(",");
+    auto right_imu_data = right_foot_file.extractDoulbeMatrix(",");
+    auto head_imu_data = head_imu_file.extractDoulbeMatrix(",");
+    auto uwb_data = uwb_file.extractDoulbeMatrix(",");
+    auto beacon_set_data = uwb_file.extractDoulbeMatrix(",");
+
+
+    //process
+
+
+}
