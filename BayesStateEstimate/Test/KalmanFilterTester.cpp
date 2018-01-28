c701c4205ea61f09ca4d2a76b66eb452d019af04
steve <551619855@qq.com>
2018-01-29 00:16:03 +0800

    some problem.

--- BayesStateEstimate/Test/KalmanFilterTester.cpp
+++ BayesStateEstimate/Test/KalmanFilterTester.cpp
@@ -13,20 +13,27 @@
 
 #include "AWF.h"
 
+#include "../BayesFilter/KalmanFilter/IMUWBKF.h"
+#include "../BayesFilter/KalmanFilter/IMUWBKF.cpp"
 
 
 /**
  *  process the imu data according to the typical sensor model
- * @param imu_data
+ * @param imu_data after preprocess time[s] acc_(x,y,z)[m*s^-2] gyr_(x,y,z)[rad*s^-1] mag_(x,y,z) pressure
  */
-void processImuData(Eigen::MatrixXd &imu_data){
+void processImuData(Eigen::MatrixXd &imu_data) {
 //    Eigen::MatrixXd tmp_data = imu_data.;
     Eigen::MatrixXd tmp_data(imu_data);
 
-    imu_data.block(0,0,imu_data.rows(),1) = tmp_data.block(0,0,imu_data.rows(),1) *1.0;
-
-    imu_data.block()
-
+    int row(imu_data.rows());
+    int col(imu_data.cols());
+    imu_data.resize(row, 1 + 3 + 3 + 3 + 1);//
+    // time
+    imu_data.block(0, 0, row, 1) = tmp_data.block(0, 0, row, 1) * 1.0;
+    imu_data.block(0, 1, row, 3) = tmp_data.block(0, 2, row, 3) * 9.81;
+    imu_data.block(0, 4, row, 3) = tmp_data.block(0, 5, row, 3) * (M_PI / 180.0);
+    imu_data.block(0, 7, row, 3) = tmp_data.block(0, 8, row, 3) * 1.0;
+    return;
 }
 
 int main(int argc, char *argv[]) {
@@ -48,8 +55,22 @@ int main(int argc, char *argv[]) {
     auto uwb_data = uwb_file.extractDoulbeMatrix(",");
     auto beacon_set_data = uwb_file.extractDoulbeMatrix(",");
 
+    assert(beacon_set_data.rows()==(uwb_data.cols()-1));
 
     //process
+    processImuData(left_imu_data);
+    processImuData(right_imu_data);
+    processImuData(head_imu_data);
+
+    Eigen::MatrixXd process_noise_matrix =
+            Eigen::MatrixXd::Identity(6,6);
+    process_noise_matrix.block(0,0,3,3) *= 0.01;
+    process_noise_matrix.block(3,3,3,3) *= (0.01 *M_PI/180.0);
+
+    Eigen::MatrixXd measurement_noise_matrix = Eigen::MatrixXd::Identity(uwb_data.cols()-1,uwb_data.cols()-1);
+
+
+    auto filter = BSE::IMUWBKFBase()
 
 
 
