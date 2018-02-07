5dadb26c31dccd73ef1730793debdb0233a41eb7
steve <551619855@qq.com>
2018-02-07 16:42:43 +0800

    begin to try new method.

--- GraphOptimization/src/DualFeetLocalization.cpp
+++ GraphOptimization/src/DualFeetLocalization.cpp
@@ -13,8 +13,9 @@
 
 
 int main(int argc, char *argv[]) {
+    std::cout.precision(30);
     // parameters
-    std::string dir_name = "/home/steve/Data/FusingLocationData/0017/";
+    std::string dir_name = "/home/steve/Data/FusingLocationData/0013/";
 
 
 
@@ -22,17 +23,61 @@ int main(int argc, char *argv[]) {
     AWF::FileReader left_foot_file(dir_name + "LEFT_FOOT.data"),
             right_foot_file(dir_name + "RIGHT_FOOT.data"),
             head_imu_file(dir_name + "HEAD.data"),
-            uwb_file(dir_name + "HEAD_UWB.data"),
+            uwb_file(dir_name + "uwb_result.csv"),
             beacon_set_file(dir_name + "beaconSet.csv");
 
-    auto left_imu_data = left_foot_file.extractDoulbeMatrix(",");
-    auto right_imu_data = right_foot_file.extractDoulbeMatrix(",");
-    auto head_imu_data = head_imu_file.extractDoulbeMatrix(",");
-    auto uwb_data = uwb_file.extractDoulbeMatrix(",");
-    auto beacon_set_data = uwb_file.extractDoulbeMatrix(",");
+    Eigen::MatrixXd left_imu_data = left_foot_file.extractDoulbeMatrix(",");
+    Eigen::MatrixXd right_imu_data = right_foot_file.extractDoulbeMatrix(",");
+    Eigen::MatrixXd head_imu_data = head_imu_file.extractDoulbeMatrix(",");
+    Eigen::MatrixXd uwb_data = uwb_file.extractDoulbeMatrix(",");
+    Eigen::MatrixXd beacon_set_data = beacon_set_file.extractDoulbeMatrix(",");
+
+    assert(beacon_set_data.rows() == (uwb_data.cols() - 1));
+
+    // get the initial pose based on uwb data.
+
+    std::cout << uwb_data.block(0, 0, 1, uwb_data.cols()) << std::endl;
+
+    auto uwb_tool = BSE::UwbTools(uwb_data,
+                                  beacon_set_data);
+
+    Eigen::MatrixXd optimize_trace = uwb_tool.uwb_position_function();
+    Eigen::Vector3d initial_pos = optimize_trace.block(0, 0, 1, 3).transpose();
+    double initial_ori = uwb_tool.computeInitialOri(optimize_trace);
+
+    std::vector<std::vector<double>> optimize_trace_vec = {{},
+                                                           {},
+                                                           {}};
+    for (int i(0); i < optimize_trace.rows(); ++i) {
+        for (int j(0); j < 3; ++j) {
+            optimize_trace_vec[j].push_back(optimize_trace(i, j));
+        }
+    }
+
+
+//    std::cout << last_uwb_err
+    auto imu_tool = BSE::ImuTools();
 
 
     //process
+    imu_tool.processImuData(left_imu_data);
+    imu_tool.processImuData(right_imu_data);
+    imu_tool.processImuData(head_imu_data);
+
+    Eigen::MatrixXd process_noise_matrix =
+            Eigen::MatrixXd::Identity(6, 6);
+    process_noise_matrix.block(0, 0, 3, 3) *= 0.1;
+    process_noise_matrix.block(3, 3, 3, 3) *= (0.1 * M_PI / 180.0);
+
+    Eigen::MatrixXd measurement_noise_matrix =
+            Eigen::MatrixXd::Identity(uwb_data.cols() - 1, uwb_data.cols() - 1);
+    measurement_noise_matrix *= 0.1;
+
+
+    Eigen::MatrixXd initial_prob_matrix = Eigen::MatrixXd::Identity(9, 9);
+    initial_prob_matrix.block(0, 0, 3, 3) *= 0.001;
+    initial_prob_matrix.block(3, 3, 3, 3) *= 0.001;
+    initial_prob_matrix.block(6, 6, 3, 3) *= 0.001 * (M_PI / 180.0);
 
 
 }
---
+++ Scripts/DataLoader.py
@@ -0,0 +1,49 @@
+# Created by steve on 18-2-7 下午4:17
+'''
+                   _ooOoo_ 
+                  o8888888o 
+                  88" . "88 
+                  (| -_- |) 
+                  O\  =  /O 
+               ____/`---'\____ 
+             .'  \\|     |//  `. 
+            /  \\|||  :  |||//  \ 
+           /  _||||| -:- |||||-  \ 
+           |   | \\\  -  /// |   | 
+           | \_|  ''\---/''  |   | 
+           \  .-\__  `-`  ___/-. / 
+         ___`. .'  /--.--\  `. . __ 
+      ."" '<  `.___\_<|>_/___.'  >'"". 
+     | | :  `- \`.;`\ _ /`;.`/ - ` : | | 
+     \  \ `-.   \_ __\ /__ _/   .-` /  / 
+======`-.____`-.___\_____/___.-`____.-'====== 
+                   `=---=' 
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ 
+         佛祖保佑       永无BUG 
+'''
+
+import numpy as np
+import scipy as sp
+from matplotlib.pylab import plt
+
+
+class DataLoader:
+    def __init__(self,
+                 dir_name='/home/steve/Data/FusingLocationData/0013/'):
+        if not dir_name[-1] is '/':
+            dir_name = dir_name + '/'
+
+        imu_left = np.loadtxt(dir_name+'LEFT_FOOT.data',delimiter=',')
+        imu_right = np.loadtxt(dir_name+'RIGHT_FOOT.data',delimiter=',')
+        imu_head = np.loadtxt(dir_name+'HEAD.data',delimiter=',')
+
+        uwb_head = np.loadtxt(dir_name + 'HEAD_UWB.data',delimiter=',')
+        beacon_set = np.loadtxt(dir_name+'beaconSet.csv',delimiter=',')
+
+        print('average time interval of left:',
+              float(imu_left[:,-1]-imu_left[:,0])/float(imu_left.shape[0]))
+
+
+
+if __name__ == '__main__':
+    dl = DataLoader()
