fea54f0dac68bbc4d0349f75d5e5f3d23bc06b18
steve <551619855@qq.com>
2018-02-13 21:04:45 +0800

    high accuracy zupt algorithm.

--- BayesStateEstimate/Test/device_test_head.h
+++ BayesStateEstimate/Test/device_test_head.h
@@ -23,30 +23,16 @@
 //
 // Created by steve on 18-2-13.
 //
+
+#ifndef COMPLEXITYPOSITIONING_DEVICE_TEST_HEAD_H
+#define COMPLEXITYPOSITIONING_DEVICE_TEST_HEAD_H
 #include <thrust/host_vector.h>
 #include <thrust/device_vector.h>
 #include <thrust/copy.h>
 #include <thrust/fill.h>
 #include <thrust/sequence.h>
 #include <iostream>
-#include "device_test_head.h"
-int test_function(){
-    thrust::device_vector<int> H(10);
-    thrust::fill_n(H.begin(),10,12);
-
-    thrust::host_vector<int> D(H.size());
-    thrust::copy(H.begin(),H.end(),D.begin());
-
-    // print D
-    for (int i = 0; i < D.size(); i++)
-        std::cout << "D[" << i << "] = " << D[i] << std::endl;
-    return 0;
-}
-
-int main(void) {
-    test_function();
 
 
-    return 0;
-}
 
+#endif //COMPLEXITYPOSITIONING_DEVICE_TEST_HEAD_H
