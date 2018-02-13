fb1b9b29513c21e5592a3f665699f11f9d5e20b6
steve <551619855@qq.com>
2018-02-13 18:53:59 +0800

    redefined KalmanFilterNonLinearBase

--- BayesStateEstimate/Test/KalmanFilterTester.cpp
+++ BayesStateEstimate/Test/KalmanFilterTester.cpp
@@ -32,7 +32,7 @@ int main(int argc, char *argv[]) {
 
     std::cout.precision(30);
     // parameters
-    std::string dir_name = "/home/steve/Data/FusingLocationData/0013/";
+    std::string dir_name = "/home/steve/Data/FusingLocationData/0012/";
 
 
 
