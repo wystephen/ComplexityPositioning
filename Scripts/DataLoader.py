b1073e34786fc5538033a1039a43a1cc535261c7
steve <551619855@qq.com>
2018-02-15 17:18:38 +0800

    add boost compute (using opencl for gup accelerate.)

--- Scripts/DataLoader.py
+++ Scripts/DataLoader.py
@@ -33,33 +33,31 @@ class DataLoader:
         if not dir_name[-1] is '/':
             dir_name = dir_name + '/'
 
-        imu_left = np.loadtxt(dir_name+'LEFT_FOOT.data',delimiter=',')
-        imu_right = np.loadtxt(dir_name+'RIGHT_FOOT.data',delimiter=',')
-        imu_head = np.loadtxt(dir_name+'HEAD.data',delimiter=',')
+        imu_left = np.loadtxt(dir_name + 'LEFT_FOOT.data', delimiter=',')
+        imu_right = np.loadtxt(dir_name + 'RIGHT_FOOT.data', delimiter=',')
+        imu_head = np.loadtxt(dir_name + 'HEAD.data', delimiter=',')
 
-        uwb_head = np.loadtxt(dir_name + 'uwb_result.csv',delimiter=',')
-        beacon_set = np.loadtxt(dir_name+'beaconSet.csv',delimiter=',')
+        uwb_head = np.loadtxt(dir_name + 'uwb_result.csv', delimiter=',')
+        beacon_set = np.loadtxt(dir_name + 'beaconSet.csv', delimiter=',')
 
         print('average time interval of left:',
-              float(imu_left[-1,1]-imu_left[0,1])/float(imu_left.shape[0]))
+              float(imu_left[-1, 1] - imu_left[0, 1]) / float(imu_left.shape[0]))
         print('average time interval of right:',
-              float(imu_right[-1,1]-imu_right[0,1])/float(imu_right.shape[0]))
+              float(imu_right[-1, 1] - imu_right[0, 1]) / float(imu_right.shape[0]))
 
         print('average time interval of head:',
-              float(imu_head[-1,1]-imu_head[0,1])/float(imu_head.shape[0]))
+              float(imu_head[-1, 1] - imu_head[0, 1]) / float(imu_head.shape[0]))
 
         plt.figure()
-        plt.plot(imu_left[1:,1]-imu_left[:-1,1], label = 'time left')
-        plt.plot(imu_right[1:,1]-imu_right[:-1,1], label = 'time right')
+        plt.plot(imu_left[1:, 1] - imu_left[:-1, 1], label='time left')
+        plt.plot(imu_right[1:, 1] - imu_right[:-1, 1], label='time right')
         # time_diff = imu_left[1:,1] - imu_left[:-1,1]
         # plt.plot(time_diff-time_diff.mean(),label='time diff')
-        plt.plot(imu_head[1:,1]-imu_head[:-1,1], label = ' time head' )
+        plt.plot(imu_head[1:, 1] - imu_head[:-1, 1], label=' time head')
 
         plt.grid()
         plt.legend()
         plt.show()
-
-
 
 
 if __name__ == '__main__':
