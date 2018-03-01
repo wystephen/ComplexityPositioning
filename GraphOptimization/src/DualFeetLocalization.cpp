fa85fff8f2e4e7b1c51edf57c47ff4ebd9422ef4
steve <551619855@qq.com>
2018-03-01 18:37:22 +0800

    add boost compute (using opencl for gup accelerate.)

--- GraphOptimization/src/DualFeetLocalization.cpp
+++ GraphOptimization/src/DualFeetLocalization.cpp
@@ -20,6 +20,21 @@
 #include "../../BayesStateEstimate/BayesFilter/KalmanFilter/IMUWBKF.cpp"
 
 
+#include "g2o/core/sparse_optimizer.h"
+#include "g2o/core/block_solver.h"
+#include "g2o/core/factory.h"
+#include "g2o/core/optimization_algorithm_levenberg.h"
+#include "g2o/solvers/csparse/linear_solver_csparse.h"
+#include "g2o/types/slam3d/types_slam3d.h"
+#include "g2o/types/slam3d_addons/types_slam3d_addons.h"
+#include "g2o/solvers/cholmod/linear_solver_cholmod.h"
+
+#include "g2o/core/robust_kernel.h"
+#include "g2o/core/robust_kernel_impl.h"
+
+#include "g2o/core/robust_kernel.h"
+#include "g2o/core/robust_kernel_factory.h"
+
 int main(int argc, char *argv[]) {
     std::cout.precision(30);
     // parameters
