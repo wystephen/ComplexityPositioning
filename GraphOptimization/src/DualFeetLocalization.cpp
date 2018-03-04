0a1f1f6253a280915f3ab23f2d82c67f858b8321
steve <551619855@qq.com>
2018-03-04 22:26:55 +0800

    max distance constrainted.

--- GraphOptimization/src/DualFeetLocalization.cpp
+++ GraphOptimization/src/DualFeetLocalization.cpp
@@ -485,7 +485,7 @@ int main(int argc, char *argv[]) {
             e->vertices()[1] = globalOptimizer.vertex(right_vertex_index-1);
 
             Eigen::Matrix<double, 1, 1> info = Eigen::Matrix<double, 1, 1>::Identity();
-            info *= 0.01;
+            info *= 0.001;
             e->setInformation(info);
 
             globalOptimizer.addEdge(e);
