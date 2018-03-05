79dc7752d852efccd8e59e36fd22ced4a62547b6
steve <551619855@qq.com>
2018-03-05 18:52:42 +0800

    max distance constrainted.

--- GraphOptimization/src/DualFeetLocalization.cpp
+++ GraphOptimization/src/DualFeetLocalization.cpp
@@ -489,7 +489,7 @@ int main(int argc, char *argv[]) {
 //                              << uwb_data(uwb_index,k)
 //                              << std::endl;
 
-                   // add_uwb_edge(uwb_data(uwb_index, k), k - 1, 0);
+                   add_uwb_edge(uwb_data(uwb_index, k), k - 1, 0);
                 }
 
             }
