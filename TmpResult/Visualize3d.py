1cd322cb5d312d36030c977b07e632c86fb740fb
steve <551619855@qq.com>
2018-03-06 19:59:54 +0800

    add visualization scripts.

--- TmpResult/Visualize3d.py
+++ TmpResult/Visualize3d.py
@@ -24,13 +24,28 @@
 
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+import os
 
 import numpy as np
 
+def plot_file(ax_handle,file_name):
+    data = np.loadtxt(file_name,delimiter=',')
+    ax_handle.plot(data[:,0],data[:,1],data[:,2],label=file_name)
+
 if __name__ == '__main__':
-    dir_name = './TmpResult/'
+    dir_name = ''
 
-    left_data = np.loadtxt(dir_name+'graph_left_pose.csv',delimiter=',')
-    right_data = np.loadtxt(dir_name+'graph_right_pose.csv',delimiter=',')
 
-    fig = plt.figu
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1,projection='3d')
+
+    for file_name in os.listdir('./'):
+        print(file_name)
+
+        if '.csv' in file_name:
+            plot_file(ax,file_name)
+
+    ax.legend()
+    ax.grid()
+
+    plt.show()
