99945a59c6fc065243fc0529dc3b14033625522c
steve <551619855@qq.com>
2018-03-08 15:36:42 +0800

    add visualization scripts.

--- BayesStateEstimate/Test/XsenseIntegrateTest.cpp
+++ BayesStateEstimate/Test/XsenseIntegrateTest.cpp
@@ -24,5 +24,55 @@
 // Created by steve on 18-3-8.
 //
 
-#include
 
+
+#include <iostream>
+#include <fstream>
+#include <thread>
+
+#include <Eigen/Dense>
+#include <Eigen/Geometry>
+//#include <AWF.h>dd
+
+
+#include "AWF.h"
+
+#include "../BayesFilter/KalmanFilter/IMUWBKF.h"
+#include "../BayesFilter/KalmanFilter/IMUWBKF.cpp"
+
+#include "../AuxiliaryTool/UwbTools.h"
+#include "../AuxiliaryTool/UwbTools.cpp"
+
+#include "../AuxiliaryTool/ImuTools.h"
+#include "../AuxiliaryTool/ImuTools.cpp"
+
+
+namespace plt = matplotlibcpp;
+
+
+int main(int argc, char *argv[]) {
+
+    std::cout.precision(30);
+
+    std::string file_name = "/home/steve/Data/XsensData/line-low.csv";
+
+    AWF::FileReader imu_file(file_name);
+
+    Eigen::MatrixXd imu_data = imu_file.extractDoulbeMatrix(",");
+
+    Eigen::MatrixXd process_noise_matrix =
+            Eigen::MatrixXd::Identity(6, 6);
+    process_noise_matrix.block(0, 0, 3, 3) *= 0.1;
+    process_noise_matrix.block(3, 3, 3, 3) *= (0.1 * M_PI / 180.0);
+
+
+    Eigen::MatrixXd initial_prob_matrix = Eigen::MatrixXd::Identity(9, 9);
+    initial_prob_matrix.block(0, 0, 3, 3) *= 0.001;
+    initial_prob_matrix.block(3, 3, 3, 3) *= 0.001;
+    initial_prob_matrix.block(6, 6, 3, 3) *= 0.001 * (M_PI / 180.0);
+
+
+    auto filter = BSE::IMUWBKFSimple(initial_prob_matrix);
+
+
+}
