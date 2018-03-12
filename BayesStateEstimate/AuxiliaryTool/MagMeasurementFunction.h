8d2435fa23d77a2efe3b0a58b30cde92b554c207
steve <551619855@qq.com>
2018-03-12 21:06:07 +0800

    Try to use new data with only imu data.

--- BayesStateEstimate/AuxiliaryTool/MagMeasurementFunction.h
+++ BayesStateEstimate/AuxiliaryTool/MagMeasurementFunction.h
@@ -26,10 +26,46 @@
 
 #ifndef COMPLEXITYPOSITIONING_MAGMEASUREMENTFUNCTION_H
 #define COMPLEXITYPOSITIONING_MAGMEASUREMENTFUNCTION_H
+
 #include <Eigen/Dense>
+#include <Eigen/Geometry>
 #include "AWF.h"
 
-class MagMeasurementFunction {
+class MagMeasurementFunction : public AWF::FunctionAbstract {
+public:
+    MagMeasurementFunction(Eigen::Vector3d mag_in_navigation_frame) : FunctionAbstract(3, 1) {
+        mag_nav_ = mag_in_navigation_frame / mag_in_navigation_frame.norm();
+
+    }
+
+    Eigen::MatrixXd compute(Eigen::MatrixXd state) {
+        Eigen::Quaterniond q = Eigen::AngleAxisd(state(6, 0), Eigen::Vector3d::UnitX()) *
+                               Eigen::AngleAxisd(state(7, 0), Eigen::Vector3d::UnitY()) *
+                               Eigen::AngleAxisd(state(8, 0), Eigen::Vector3d::UnitZ());
+        return q.inverse() * mag_nav_;
+
+
+    }
+
+    Eigen::MatrixXd operator()(std::vector<Eigen::MatrixXd> in_vec){
+        return compute(in_vec[0]);
+    }
+
+    std::vector<Eigen::MatrixXd> derivative(Eigen::MatrixXd in1){
+
+        return d(compress(in1));
+    }
+
+//    std::vector<Eigen::MatrixXd> minimize_error()
+
+    std::vector<Eigen::MatrixXd> compress(Eigen::MatrixXd input_state){
+        std::vector<Eigen::MatrixXd> t = {};
+        t.push_back(input_state);
+        return t;
+    }
+
+
+    Eigen::Vector3d mag_nav_ = Eigen::Vector3d(0, 0, 1.0);
 
 };
 
