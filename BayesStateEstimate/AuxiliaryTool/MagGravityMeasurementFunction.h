4c44e87ade3a38b47e0c54c05a9de0943bacc5d9
steve <551619855@qq.com>
2018-03-13 15:18:30 +0800

    ...try to correct ver

---
+++ BayesStateEstimate/AuxiliaryTool/MagGravityMeasurementFunction.h
@@ -0,0 +1,52 @@
+/** 
+                   _ooOoo_ 
+                  o8888888o 
+                  88" . "88 
+                  (| -_- |) 
+                  O\  =  /O 
+               ____/`---'\____ 
+             .'  \\|     |//  `. 
+            /  \\|||  :  |||//  \ 
+           /  _||||| -:- |||||-  \ 
+           |   | \\\  -  /// |   | 
+           | \_|  ''\---/''  |   | 
+           \  .-\__  `-`  ___/-. / 
+         ___`. .'  /--.--\  `. . __ 
+      ."" '<  `.___\_<|>_/___.'  >'"". 
+     | | :  `- \`.;`\ _ /`;.`/ - ` : | | 
+     \  \ `-.   \_ __\ /__ _/   .-` /  / 
+======`-.____`-.___\_____/___.-`____.-'====== 
+                   `=---=' 
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ 
+         佛祖保佑       永无BUG 
+*/
+//
+// Created by steve on 18-3-13.
+//
+
+#ifndef COMPLEXITYPOSITIONING_MAGGRAVITYMEASUREMENTFUNCTION_H
+#define COMPLEXITYPOSITIONING_MAGGRAVITYMEASUREMENTFUNCTION_H
+
+#include <Eigen/Dense>
+#include <Eigen/Geometry>
+
+#include "AWF.h"
+
+class MagGravityMeasurementFunction:public MagGravityMeasurementFunction {
+public:
+    MagGravityMeasurementFunction():MagGravityMeasurementFunction(){
+
+    }
+
+    Eigen::MatrixXd compute(Eigen::MatrixXd state){
+
+
+    }
+
+
+
+
+};
+
+
+#endif //COMPLEXITYPOSITIONING_MAGGRAVITYMEASUREMENTFUNCTION_H
