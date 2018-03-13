1cbba1691111b212168b5d9e46684e4dcf6b403e
steve <551619855@qq.com>
2018-03-13 16:45:29 +0800

    ...try to correct ver

--- BayesStateEstimate/AuxiliaryTool/MagGravityMeasurementFunction.h
+++ BayesStateEstimate/AuxiliaryTool/MagGravityMeasurementFunction.h
@@ -29,21 +29,41 @@
 
 #include <Eigen/Dense>
 #include <Eigen/Geometry>
+#include "MagMeasurementFunction.h"
 
 #include "AWF.h"
 
-class MagGravityMeasurementFunction:public MagGravityMeasurementFunction {
+class MagGravityMeasurementFunction : public MagMeasurementFunction {
 public:
-    MagGravityMeasurementFunction():MagGravityMeasurementFunction(){
+    MagGravityMeasurementFunction() : MagMeasurementFunction() {
 
     }
 
-    Eigen::MatrixXd compute(Eigen::MatrixXd state){
+    /**
+     *  compute
+     * @param state
+     * @return
+     */
+    Eigen::MatrixXd compute(Eigen::MatrixXd state) {
+        Eigen::Quaterniond q = Eigen::AngleAxisd(state(6, 0), Eigen::Vector3d::UnitX()) *
+                               Eigen::AngleAxisd(state(7, 0), Eigen::Vector3d::UnitY()) *
+                               Eigen::AngleAxisd(state(8, 0), Eigen::Vector3d::UnitZ());
 
-
+        Eigen::Matrix<double, 6, 1> out;
+        out.block(0, 0, 3, 1) = q.inverse() * gravity_nav_;
+        out.block(3, 0, 3, 1) = q.inverse() * mag_nav_;
+        return out;
     }
 
+    Eigen::Vector3d gravity_nav_ = Eigen::Vector3d(0, 0, 9.81);
 
+    const Eigen::Vector3d &getGravity_nav_() const {
+        return gravity_nav_;
+    }
+
+    void setGravity_nav_(const Eigen::Vector3d &gravity_nav_) {
+        MagGravityMeasurementFunction::gravity_nav_ = gravity_nav_;
+    }
 
 
 };
