0507b6c8d5d7729203d9e69bf0439eb78912ba87
steve <551619855@qq.com>
2018-03-16 14:10:17 +0800

    try to use new data.

--- BayesStateEstimate/BayesFilter/KalmanFilter/KFComplex.h
+++ BayesStateEstimate/BayesFilter/KalmanFilter/KFComplex.h
@@ -206,9 +206,9 @@ namespace BSE {
             omega *= -1.0;
             rotation_m = (Eigen::Matrix3d::Identity() - omega) * rotation_m;
 
-            rotation_q_ = Eigen::Quaterniond(rotation_m);
+//            rotation_q_ = Eigen::Quaterniond(rotation_m);
 
-//            rotation_q_ = rotation_q_ * delta_q;
+            rotation_q_ = rotation_q_ * delta_q;
             rotation_q_.normalize();
             state_x_.block(6,0,3,1) = rotation_q_.toRotationMatrix().eulerAngles(0,1,2);
             return;
