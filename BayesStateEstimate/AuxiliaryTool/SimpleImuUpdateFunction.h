42adc93ee0b3f7eaeb90d68aec1f7590821ee817
steve <551619855@qq.com>
2018-03-20 19:35:13 +0800

    use so3 .

--- BayesStateEstimate/AuxiliaryTool/SimpleImuUpdateFunction.h
+++ BayesStateEstimate/AuxiliaryTool/SimpleImuUpdateFunction.h
@@ -56,7 +56,7 @@ namespace BSE {
 
             Sophus::SO3 rotation = Sophus::SO3::exp(state.block(6,0,3,1));
             Eigen::Vector3d gyr = input.block(3, 0, 3, 1) * time_interval_ ;
-            std::cout << time_interval_ << std::endl;
+//            std::cout << time_interval_ << std::endl;
             assert(time_interval_>0.0 && time_interval_ < 0.1);
 
             if (input.block(3, 0, 3, 1).norm() > 1e-6) {
