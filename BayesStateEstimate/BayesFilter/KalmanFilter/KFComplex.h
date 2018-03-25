e8804745dc59ce4e50a1e44139f7fb927ea13fa3
steve <551619855@qq.com>
2018-03-25 14:00:02 +0800

    start change optimization.

--- BayesStateEstimate/BayesFilter/KalmanFilter/KFComplex.h
+++ BayesStateEstimate/BayesFilter/KalmanFilter/KFComplex.h
@@ -204,8 +204,7 @@ namespace BSE {
 //            state_x_.block(6, 0, 3, 1) = BSE::ImuTools::angleAdd(state_x_.block(6, 0, 3, 1),
 //                                                                 dX_.block(6, 0, 3, 1));
             rbn_ = rbn_ * Sophus::SO3::exp(dX_.block(6, 0, 3, 1));
-            state_x_.block(6,0,3,1) = rbn_.log();
-
+            state_x_.block(6, 0, 3, 1) = rbn_.log();
 
 
         }
