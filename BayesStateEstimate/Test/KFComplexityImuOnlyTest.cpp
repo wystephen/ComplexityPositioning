71176b118cc2c9b90d58100709d9a7a3e83c5ab5
steve <551619855@qq.com>
2018-04-03 16:47:04 +0800

    hard to use.

--- BayesStateEstimate/Test/KFComplexityImuOnlyTest.cpp
+++ BayesStateEstimate/Test/KFComplexityImuOnlyTest.cpp
@@ -165,7 +165,7 @@ int main(int argc, char *argv[]) {
             auto complex_state = filter_complex.StateTransIMU(imu_data.block(i, 1, 1, 6).transpose(),
                                                               process_noise_matrix);
             filter_complex.MeasurementAngleCorrect(imu_data.block(i, 7, 1, 3).transpose(),
-                                                   Eigen::Matrix3d::Identity() * 0.001);
+                                                   Eigen::Matrix3d::Identity() * 0.0001);
 
             double uwb_index = 0;
             /// uwb measurement
