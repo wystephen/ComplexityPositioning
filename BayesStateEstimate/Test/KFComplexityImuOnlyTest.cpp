a8d7201be9a43f7d0e5acd271b547314e0715a53
steve <551619855@qq.com>
2018-04-06 13:46:11 +0800

    use new version of sophus.

--- BayesStateEstimate/Test/KFComplexityImuOnlyTest.cpp
+++ BayesStateEstimate/Test/KFComplexityImuOnlyTest.cpp
@@ -177,8 +177,8 @@ int main(int argc, char *argv[]) {
 
             auto complex_state = filter_complex.StateTransIMU(imu_data.block(i, 1, 1, 6).transpose(),
                                                               process_noise_matrix);
-//            filter_complex.MeasurementAngleCorrect(imu_data.block(i, 7, 1, 3).transpose(),
-//                                                   Eigen::Matrix3d::Identity() * 0.00000026);
+            filter_complex.MeasurementAngleCorrect(imu_data.block(i, 7, 1, 3).transpose(),
+                                                   Eigen::Matrix3d::Identity() * 0.00000026);
 
             double uwb_index = 0;
             /// uwb measurement
