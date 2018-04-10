1ea635f5bce1c8feebbc29a40365c55dc890f283
steve <551619855@qq.com>
2018-04-10 08:15:41 +0800

    try to build UKF for 15 state.(some problem...)

--- BayesStateEstimate/Test/KFComplexityImuOnlyTest.cpp
+++ BayesStateEstimate/Test/KFComplexityImuOnlyTest.cpp
@@ -63,25 +63,11 @@ int main(int argc, char *argv[]) {
 	AWF::FileReader left_foot_file(dir_name + "LEFT_FOOT.data"),
 			right_foot_file(dir_name + "RIGHT_FOOT.data"),
 			head_imu_file(dir_name + "HEAD.data");
-//            uwb_file(dir_name + "uwb_result.csv"),
-//            beacon_set_file(dir_name + "beaconSet.csv");
 
 	Eigen::MatrixXd left_imu_data = left_foot_file.extractDoulbeMatrix(",");
 	Eigen::MatrixXd right_imu_data = right_foot_file.extractDoulbeMatrix(",");
 	Eigen::MatrixXd head_imu_data = head_imu_file.extractDoulbeMatrix(",");
-//    Eigen::MatrixXd uwb_data = uwb_file.extractDoulbeMatrix(",");
-//    Eigen::MatrixXd beacon_set_data = beacon_set_file.extractDoulbeMatrix(",");
 
-//    assert(beacon_set_data.rows() == (uwb_data.cols() - 1));
-
-	// get the initial pose based on uwb data.
-
-//    std::cout << uwb_data.block(0, 0, 1, uwb_data.cols()) << std::endl;
-
-//    auto uwb_tool = BSE::UwbTools(uwb_data,
-//                                  beacon_set_data);
-
-//    Eigen::MatrixXd optimize_trace = uwb_tool.uwb_position_function();
 	Eigen::Vector3d initial_pos = Eigen::Vector3d(0, 0, 0);
 	double initial_ori = 0.0;
 
@@ -108,8 +94,6 @@ int main(int argc, char *argv[]) {
 	initial_prob_matrix.block(0, 0, 3, 3) *= 0.05;
 	initial_prob_matrix.block(3, 3, 3, 3) *= 0.05;
 	initial_prob_matrix.block(6, 6, 3, 3) *= 0.05 * (M_PI / 180.0);
-//    initial_prob_matrix.block(9, 9, 3, 3) *= 0.001;
-//    initial_prob_matrix.block(12, 12, 3, 3) *= 0.001 * (M_PI / 180.0);
 
 
 
