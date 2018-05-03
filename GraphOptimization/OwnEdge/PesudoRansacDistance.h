455602e7288f2a3b7647fe77af5962a6363d42f7
steve <551619855@qq.com>
2018-05-03 16:51:07 +0800

    try to use accpetable fusion result.

--- GraphOptimization/OwnEdge/PesudoRansacDistance.h
+++ GraphOptimization/OwnEdge/PesudoRansacDistance.h
@@ -4,54 +4,56 @@
 
 #ifndef COMPLEXITYPOSITIONING_PESUDORANSACDISTANCE_H
 #define COMPLEXITYPOSITIONING_PESUDORANSACDISTANCE_H
+
 #include "DistanceEdge.h"
 //#include "DistanceEdge.cpp"
 
-class PesudoRansacDistance:public DistanceEdge {
+class PesudoRansacDistance : public DistanceEdge {
 public:
-	PesudoRansacDistance(){
+	PesudoRansacDistance() {
 
 	}
 
 	bool ransac_flag_ = false;
 	double ransac_threshold_ = 10.0;
 
-	virtual void computeError(){
-			g2o::VertexSE3 *from = static_cast<g2o::VertexSE3 *>(_vertices[0]);
-			g2o::VertexSE3 *to = static_cast<g2o::VertexSE3 *>(_vertices[1]);
+	virtual void computeError() {
+		g2o::VertexSE3 *from = static_cast<g2o::VertexSE3 *>(_vertices[0]);
+		g2o::VertexSE3 *to = static_cast<g2o::VertexSE3 *>(_vertices[1]);
 
-			double p1[10], p2[10];
-			from->getEstimateData(p1);
-			to->getEstimateData(p2);
+		double p1[10], p2[10];
+		from->getEstimateData(p1);
+		to->getEstimateData(p2);
 
-			double dis_2 = (p1[0] - p2[0]) * (p1[0] - p2[0]) +
-			               (p1[1] - p2[1]) * (p1[1] - p2[1]) +
-			               (p1[2] - p2[2]) * (p1[2] - p2[2]);
-			double dis = sqrt(dis_2);
-			dis = (pow(dis-_measurement,2.0));
-
-
-			double error = dis;
+		double dis_2 = (p1[0] - p2[0]) * (p1[0] - p2[0]) +
+		               (p1[1] - p2[1]) * (p1[1] - p2[1]) +
+		               (p1[2] - p2[2]) * (p1[2] - p2[2]);
+		double dis = sqrt(dis_2);
+//			dis = (pow(dis-_measurement,2.0));
 
 
-			if(dis-_measurement<0.0){
-				// measurements bigger than theory distance
-				error = dis;
+		double error = dis;
 
-			}else{
-				error = dis *2.0;
-			}
+
+		if (_measurement > dis) {
+			// measurements bigger than theory distance
+			error =  _measurement-dis;
+
+		} else {
+			error = dis-_measurement;
+			error = error * 2.0;
+		}
 //        std::cout << dis_2 << std::endl;
-		if(ransac_flag_){
+		if (ransac_flag_) {
 			// ransac model
-			if( dis > ransac_threshold_){
+			if (dis > ransac_threshold_) {
 
-				_error(0,0) = 0.0;
-			}else{
-				_error(0,0)=error;
+				_error(0, 0) = 0.0;
+			} else {
+				_error(0, 0) = error;
 			}
 
-		}else{
+		} else {
 
 			_error(0, 0) = error;
 		}
