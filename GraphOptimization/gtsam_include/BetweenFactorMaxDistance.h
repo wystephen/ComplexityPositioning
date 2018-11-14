4266b7395c827876cafe6be166ef12a4a2f8dfbb
steve <551619855@qq.com>
2018-11-14 15:22:46 +0800

    located error segmention fault....in gtsam library.(version 4.0).

---
+++ GraphOptimization/gtsam_include/BetweenFactorMaxDistance.h
@@ -0,0 +1,52 @@
+/** 
+                   _ooOoo_ 
+                  o8888888o 
+                  88" . "88 
+                  (| -_- |) 
+                  O\  =  /O 
+               ____/`---'\____ 
+             .'  \\|     |//  `. 
+            /  \\|||  :  |||//  \ 
+           /  _||||| -:- |||||-  \ 
+           |   | \\\  -  /// |   | 
+           | \_|  ''\---/''  |   | 
+           \  .-\__  `-`  ___/-. / 
+         ___`. .'  /--.--\  `. . __ 
+      ."" '<  `.___\_<|>_/___.'  >'"". 
+     | | :  `- \`.;`\ _ /`;.`/ - ` : | | 
+     \  \ `-.   \_ __\ /__ _/   .-` /  / 
+======`-.____`-.___\_____/___.-`____.-'====== 
+                   `=---=' 
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ 
+         佛祖保佑       永无BUG 
+*/
+//
+// Created by steve on 11/14/18.
+//
+
+#ifndef COMPLEXITYPOSITIONING_BETWEENFACTORMAXDISTANCE_H
+#define COMPLEXITYPOSITIONING_BETWEENFACTORMAXDISTANCE_H
+
+
+#include <gtsam/slam/BetweenFactor.h>
+#include <gtsam/geometry/Pose3.h>
+
+namespace gtsam {
+
+//	struct BetweenFactorMaxDistance : public BetweenFactor<Pose3> {
+//		BetweenFactorMaxDistance(Key key1,
+//		                         Key key2,
+//		                         const double threshold,
+//		                         double sigma) :
+//				BetweenFactor<Pose3>(key1, key2,)
+//
+//
+//		~BetweenFactorMaxDistance() {
+//
+//		}
+//
+//	};
+
+};
+
+#endif //COMPLEXITYPOSITIONING_BETWEENFACTORMAXDISTANCE_H
