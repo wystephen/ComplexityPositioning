bcc01b1307a8105943463caae5d0c30058007524
steve <551619855@qq.com>
2019-01-05 17:00:13 +0800

    s

---
+++ GraphOptimization/gtsam_src/PDR_UWB_Robust.cpp
@@ -0,0 +1,170 @@
+//
+// Created by steve on 1/5/19.
+//
+
+
+
+#include <iostream>
+#include <fstream>
+
+
+#include <thread>
+#include <AWF.h>
+
+
+#include "AWF.h"
+
+
+#include "BSE.h"
+
+#include <Eigen/Eigen>
+
+
+#include <gtsam/slam/PriorFactor.h>
+#include <gtsam/slam/BetweenFactor.h>
+#include <gtsam/geometry/Pose2.h>
+#include <gtsam/nonlinear/ISAM2.h>
+#include <gtsam/nonlinear/NonlinearFactorGraph.h>
+#include <gtsam/inference/Symbol.h>
+
+#include <gtsam/slam/RangeFactor.h>
+
+using namespace gtsam;
+using namespace std;
+
+using symbol_shorthand::X;// pose2(x,y,theta)
+
+using symbol_shorthand::L;//landmark (uwb beacon)
+
+
+namespace plt=matplotlibcpp;
+
+int main() {
+	std::string dir_name = "/home/steve/Data/PDRUWBRobust/";
+
+	auto logger_ptr = AWF::AlgorithmLogger::getInstance();
+
+	AWF::FileReader beacon_set_file(dir_name + "beacon_coordinate.csv"),
+			pdr_file(dir_name + "pdr_result.csv"),
+			uwb_file(dir_name + "uwb_noise.csv");
+
+	Eigen::MatrixXd pdr_data = pdr_file.extractDoulbeMatrix(",");
+	Eigen::MatrixXd beacon_set = beacon_set_file.extractDoulbeMatrix(",");
+	Eigen::MatrixXd uwb_data = uwb_file.extractDoulbeMatrix(",");
+
+
+	// graph and values
+	NonlinearFactorGraph graph;
+	Values values;
+
+	int pose_counter = 0;
+
+
+	// define noise model.
+	auto pose2_prior_noise = noiseModel::Diagonal::Sigmas(Vector3(0.1, 0.1, 0.1*M_PI));
+//	auto beacon_set_noise = noiseModel::Constrained::All(2);
+	auto beacon_set_noise = noiseModel::Constrained::Sigmas(Vector2(0.1,0.1));
+	auto odometry_noise = noiseModel::Diagonal::Sigmas(Vector3(0.5, 0.5, 0.21));
+	auto base_range_noise = noiseModel::Diagonal::Sigmas((Vector(1)<<1.0).finished());
+	auto range_noise = noiseModel::Robust::Create(noiseModel::mEstimator::Tukey::Create(1.0),
+			base_range_noise);  //noiseModel::Robust(noise::noiseModel::Isotropic::Sigma(1,0.1));
+
+
+	// add prior constraint
+	Pose2 initial_pose = Pose2(0.15,5.0,0.0);
+	values.insert(X(pose_counter),initial_pose);
+	graph.add(
+			PriorFactor<Pose2>(X(pose_counter),initial_pose,pose2_prior_noise)
+			);
+
+	for(int i=0;i<beacon_set.rows();++i){
+		Point2 beacon_set_point = Point2(beacon_set(i,0),beacon_set(i,1));
+		values.insert(L(i),beacon_set_point);
+		graph.add(PriorFactor<Point2>(L(i),beacon_set_point,beacon_set_noise));
+	}
+
+	//initial isam2
+	ISAM2Params params;
+	ISAM2 isam2(params);
+
+	isam2.update(graph,values);
+	isam2.update();
+
+	graph.resize(0);
+	values.clear();
+
+
+	int uwb_index =0;
+
+	// positioning
+	for(int i=0;i<pdr_data.rows();++i){
+		pose_counter++;
+		values.insert(X(pose_counter),initial_pose);
+		graph.add(
+				BetweenFactor<Pose2>(
+						X(pose_counter-1),X(pose_counter),
+						Pose2(pdr_data(i,1),0.0,pdr_data(i,3)),
+						odometry_noise
+						)
+				);
+		while(uwb_data(uwb_index,0)<pdr_data(i,0)){
+
+			for(int k=0;k<beacon_set.rows();++k){
+				if(uwb_data(uwb_index,k+1)>0.0){
+					//insert uwb range constraint
+					graph.add(
+							RangeFactor<Pose2,Point2>(
+									X(pose_counter),
+									L(k),
+									uwb_data(uwb_index,k+1),
+									range_noise
+									)
+							);
+//					printf("beacon:%d Range:%f\n",k,uwb_data(uwb_index,k+1));
+				}
+			}
+
+			uwb_index ++;
+
+		}
+		try{
+		isam2.update(graph,values);
+		isam2.update();
+		}catch (std::exception &e){
+			std::cout << e.what() << std::endl;
+		}
+
+
+
+		graph.resize(0);
+		values.clear();
+
+		initial_pose = isam2.calculateBestEstimate().at<Pose2>(X(pose_counter));
+		logger_ptr->addTraceEvent("trace","real_time",
+				Eigen::Vector2d(initial_pose.x(),initial_pose.y()));
+
+
+	}
+
+	for(int i=0;i<pose_counter;++i){
+		initial_pose = isam2.calculateBestEstimate().at<Pose2>(X(i));
+		logger_ptr->addTraceEvent("trace","final_result",
+				Eigen::Vector2d(initial_pose.x(),initial_pose.y()));
+	}
+
+
+	logger_ptr->outputAllEvent(true);
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
---
+++ Scripts/test_PDRUWB_data.py
@@ -0,0 +1,43 @@
+import numpy as np
+import scipy as sp
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+	pdr_data = np.loadtxt('/home/steve/Data/PDRUWBRobust/pdr_result.csv', delimiter=',')
+
+	from array import array
+	import math
+
+	trace = np.zeros([pdr_data.shape[0], 3])
+
+	initial_pos = np.asarray((0.0, 0.0, 0.0))
+
+	for i in range(pdr_data.shape[0]):
+
+		trace[i, 2] = initial_pos[2] + pdr_data[i, 3]
+
+		while trace[i, 2] > np.pi:
+			trace[i, 2] -= (2.0 * np.pi)
+
+		while trace[i, 2] < -np.pi:
+			trace[i, 2] += (2.0 * np.pi)
+
+		dx = pdr_data[i, 1] * math.sin(trace[i, 2])
+		dy = pdr_data[i, 1] * math.cos(trace[i, 2])
+		# dx = pdr_data[]
+
+		trace[i, 0] = initial_pos[0] + dx
+		trace[i, 1] = initial_pos[1] + dy
+
+
+
+		initial_pos = trace[i, :]
+
+	plt.figure()
+	plt.plot(trace[:, 0], trace[:, 1])
+	plt.grid()
+
+	plt.figure()
+	plt.plot(trace[:, 2])
+	plt.grid()
+	plt.show()
